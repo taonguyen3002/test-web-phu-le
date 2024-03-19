7618d9b22e3640047f2e566da87866105c2b1e3f
tao nguyen <tao nguyen>
2024-03-19 21:36:41 +0700

    update

--- js/rewrite_subdomain.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var currentPath = window.location.pathname;
-  if (currentPath.endsWith(".html") && currentPath !== "/index.html") {
-    var newPath = currentPath.slice(0, -5); // Loại bỏ ".html"
-    window.location.replace(newPath);
-  }
-});
